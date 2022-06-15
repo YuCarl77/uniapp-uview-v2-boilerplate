adc19c5fd5eb215bdf150b5636d180482bd13b22
yucarl77 <yucarl77@foxmail.com>
2022-06-15 17:33:06 +0800

    refactor: 深色模式调整

--- src/components/common/SwitchDark.vue
+++ src/components/common/SwitchDark.vue
@@ -1,41 +1,60 @@
 <template>
-  <u-cell-group class="switch-dark" :border="false">
-    <u-cell title="深色模式">
-      <u-switch slot="value" :value="darkMode" @change="setDarkMode" />
-    </u-cell>
-  </u-cell-group>
+  <!-- 不知道为什么微信小程序很多改不了样式要套一层,哪怕给了样式隔离 -->
+  <view class="switch-dark">
+    <u-cell-group :border="false">
+      <u-cell title="深色模式">
+        <u-switch slot="value" :value="darkMode" @change="setDarkMode" />
+      </u-cell>
+    </u-cell-group>
+  </view>
 </template>
 
 <script>
 import { mapState, mapMutations } from "vuex";
 export default {
   name: "SwitchDark",
+  options: {
+    styleIsolation: "shared",
+    virtualHost: true,
+  },
+  data() {
+    return {
+      media: null,
+    };
+  },
   computed: {
     ...mapState("app", ["darkMode"]),
   },
   methods: {
     ...mapMutations("app", ["setDarkMode"]),
-    // 监听深色模式改变
+    // 深色模式改变事件
     onDarkChange(event) {
-      console.log("darkmode change", new Date());
+      console.log("darkmode change", new Date(), event);
       const isDark = event.matches;
       this.setDarkMode(isDark);
       document.body.classList[isDark ? "add" : "remove"]("dark");
+      // 未测试, 尝试设置app的状态栏文字颜色
+      // #ifdef APP-PLUS
+      plus.navigator.setStatusBarStyle(isDark ? "dark" : "light");
+      // #endif
     },
-    listenDarkChange() {
-      window
-        ?.matchMedia("(prefers-color-scheme: dark)")
-        ?.addEventListener("change", this.onDarkChange, true);
+    // 监听深色模式改变
+    listenDarkChange(init) {
+      if (!window) return;
+      const media = window.matchMedia("(prefers-color-scheme: dark)");
+      if (init && this.darkMode === null) this.onDarkChange(media);
+      media.addEventListener("change", this.onDarkChange, true);
+      this.media = media;
     },
+    // 销毁事件监听
     // 模块热更新好像不会触发销毁事件?好像会出现多次
     destoryDarkChange() {
-      window
-        ?.matchMedia("(prefers-color-scheme: dark)")
-        ?.removeEventListener("change", this.onDarkChange, true);
+      this.media.removeEventListener("change", this.onDarkChange, true);
     },
   },
   mounted() {
-    this.listenDarkChange();
+    //小程序没法监听深色变化
+    this.listenDarkChange(true);
   },
   beforeDestroy() {
     this.destoryDarkChange();
--- src/store/modules/app.js
+++ src/store/modules/app.js
@@ -2,7 +2,7 @@ export default {
   // 可以用于存储有关整个项目的状态管理, 比如tabbar/navbar/router
   state: () => ({
     // lastRoute: null, // 比如可以做一个记录上一次跳转的路由地址信息, 用于用户登录后，重回该页面
-    darkMode: false, // 是否开启深色模式
+    darkMode: null, // 是否开启深色模式
     navbarHeight: null, // 导航栏的高度,用于sticky组件的吸顶
     mescroll: new Map(), // 用于存储每个路由对应名字的实例, 暂且只支持一个页面只有一个下拉刷新、上拉加载组件
   }),
