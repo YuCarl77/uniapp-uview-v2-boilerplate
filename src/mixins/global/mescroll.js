eab9992e11af9608c1d3c060b3c00cf19970c7d9
yucarl77 <yucarl77@foxmail.com>
2022-06-17 11:51:58 +0800

    fix: 修复小程序端不触发触底加载问题

--- src/mixins/global/mescroll.js
+++ src/mixins/global/mescroll.js
@@ -1,15 +1,14 @@
+import { mapGetters } from "vuex";
 export default {
   computed: {
-    getMescroll() {
-      return this.$store.getters["app/getMescrollByName"](this.$Route.name);
-    },
+    ...mapGetters("app", ["getMescrollByName"]),
   },
   // 注册列表滚动事件,用于判定在顶部可下拉刷新,在指定位置可显示隐藏回到顶部按钮 (此方法为页面生命周期,无法在子组件中触发, 仅在mescroll-body生效)
   onPageScroll(e) {
-    this.getMescroll && this.getMescroll.onPageScroll(e);
+    this.getMescrollByName(this.$Route.name)?.onPageScroll(e);
   },
   // 注册滚动到底部的事件,用于上拉加载 (此方法为页面生命周期,无法在子组件中触发, 仅在mescroll-body生效)
   onReachBottom() {
-    this.getMescroll && this.getMescroll.onReachBottom();
+    this.getMescrollByName(this.$Route.name)?.onReachBottom();
   },
 };
